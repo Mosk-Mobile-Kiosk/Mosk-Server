b0e57e6a278e214505aeae67e184d33a8fc69b59
Hounds1 <zoodisx@gmail.com>
2023-05-05 22:43:55 +0900

    Feat : File IO 관련 디렉토리 검사 및 생성기능 추가, 기존 선언된 상수 제거 및 열거형 상수 생성, 현재 프로젝트 경로를 가져올 수 있는 Bean 생성
    - 서버 가동시 최초 1회에 한해 정해진 경로(프로젝트 상위 경로 예를 들어, C:\Users\bae\Desktop\mosk\Mosk-Server에 프로젝트가 존재한다면 상위 경로는 C:\Users\bae\Desktop\mosk 가 됩니다.)에 디렉토리가 존재하는지 검사하고 없다면 생성. 이미 존재할 시 다음의 메세지를 출력 ValidateFileDirExists is throws message [이미 디렉토리가 존재하거나 생성에 실패했습니다.]
    - 기존 해당하는 클래스에 생성된 상수(예를 들어, FILE_DIR 등) 제거 및 Enum 클래스 생성
    - 현재 프로젝트의 경로를 가져오는 bean 생성

---
+++ src/main/java/team/mosk/api/server/global/common/ValidateFileDirExists.java
@@ -0,0 +1,45 @@
+package team.mosk.api.server.global.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import team.mosk.api.server.global.vo.NowProjectPathIs;
+import team.mosk.api.server.global.vo.PathValues;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@Component
+@Slf4j
+public class ValidateFileDirExists {
+    private static final String ALREADY_EXIST_DIR_OR_ERROR = "이미 디렉토리가 존재하거나 생성에 실패했습니다.";
+
+    @PostConstruct
+    public void validateDir() {
+        log.info("validateDir has starting work");
+        String ORIGIN_PATH = NowProjectPathIs.getPath();
+
+        String basicPath
+                = ORIGIN_PATH.substring(0, ORIGIN_PATH.lastIndexOf("\\")) + PathValues.BASIC_DIR.info();
+
+        String filePath
+                = ORIGIN_PATH.substring(0, ORIGIN_PATH.lastIndexOf("\\")) + PathValues.FILE_DIR.info();
+
+        File basicPathDir = new File(basicPath);
+        boolean firResult = createDirIfNotExist(basicPathDir);
+        if (!firResult) {
+            log.info("ValidateFileDirExists is throws message [{}]", ALREADY_EXIST_DIR_OR_ERROR);
+        }
+
+        File filePathDir = new File(filePath);
+        boolean secResult = createDirIfNotExist(filePathDir);
+        if (!secResult) {
+            log.info("ValidateFileDirExists is throws message [{}]", ALREADY_EXIST_DIR_OR_ERROR);
+        }
+    }
+
+    public boolean createDirIfNotExist(final File dir) {
+        if (dir.exists() && dir.isDirectory()) {
+            return false;
+        } else return dir.mkdirs();
+    }
+}
