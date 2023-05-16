5952315d5e94367f1e2f3bc1f89d1f6d6113bd82
Hounds1 <zoodisx@gmail.com>
2023-05-16 11:25:04 +0900

    Refactor : Option / OptionGroupController return 값 변경
    - Option / OptionGroupController => return null에서 정상 리턴으로 변경

--- src/main/java/team/mosk/api/server/domain/option/controller/OptionController.java
+++ src/main/java/team/mosk/api/server/domain/option/controller/OptionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import team.mosk.api.server.domain.option.dto.CreateOptionRequest;
 import team.mosk.api.server.domain.option.dto.OptionResponse;
 import team.mosk.api.server.domain.option.dto.UpdateOptionRequest;
+import team.mosk.api.server.domain.option.service.OptionService;
 import team.mosk.api.server.global.common.ApiResponse;
 import team.mosk.api.server.global.security.principal.CustomUserDetails;
 
@@ -20,24 +21,27 @@ import static org.springframework.http.HttpStatus.*;
 @Slf4j
 public class OptionController {
 
+    private final OptionService optionService;
+
     @PostMapping("/options")
     @ResponseStatus(CREATED)
     public ApiResponse<OptionResponse> create(@Validated @RequestBody CreateOptionRequest request,
                                               @AuthenticationPrincipal CustomUserDetails details) {
-        return null;
+        return ApiResponse.of(CREATED, optionService.create(request.toEntity(), request.getOptionGroupId(), details.getId()));
     }
 
     @PutMapping("/options")
     @ResponseStatus(OK)
     public ApiResponse<OptionResponse> update(@Validated @RequestBody UpdateOptionRequest request,
                                               @AuthenticationPrincipal CustomUserDetails details) {
-        return null;
+        return ApiResponse.ok(optionService.update(request, details.getId()));
     }
 
     @DeleteMapping("/options/{optionId}")
     @ResponseStatus(NO_CONTENT)
     public ApiResponse<Void> delete(@PathVariable Long optionId,
                                     @AuthenticationPrincipal CustomUserDetails details) {
+        optionService.delete(optionId, details.getId());
         return ApiResponse.of(NO_CONTENT, null);
     }
 }
--- src/main/java/team/mosk/api/server/domain/option/controller/OptionGroupController.java
+++ src/main/java/team/mosk/api/server/domain/option/controller/OptionGroupController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import team.mosk.api.server.domain.option.dto.CreateOptionGroupRequest;
 import team.mosk.api.server.domain.option.dto.OptionGroupResponse;
 import team.mosk.api.server.domain.option.dto.UpdateOptionGroupRequest;
+import team.mosk.api.server.domain.option.service.OptionGroupService;
 import team.mosk.api.server.global.common.ApiResponse;
 import team.mosk.api.server.global.security.principal.CustomUserDetails;
 
@@ -20,24 +21,29 @@ import static org.springframework.http.HttpStatus.*;
 @Slf4j
 public class OptionGroupController {
 
-    @PostMapping("/opgroups")
+    private final OptionGroupService optionGroupService;
+
+    @PostMapping("/optiongroups")
     @ResponseStatus(CREATED)
     public ApiResponse<OptionGroupResponse> create(@Validated @RequestBody CreateOptionGroupRequest request,
                                                    @AuthenticationPrincipal CustomUserDetails details) {
-        return null;
+        return ApiResponse.of(CREATED, optionGroupService.create(request.toEntity(), request.getProductId(), details.getId()));
     }
 
-    @PutMapping("/opgroups")
+    @PutMapping("/optiongroups")
     @ResponseStatus(OK)
     public ApiResponse<OptionGroupResponse> update(@Validated @RequestBody UpdateOptionGroupRequest request,
                                                    @AuthenticationPrincipal CustomUserDetails details) {
-        return null;
+        return ApiResponse.ok(optionGroupService.update(request, details.getId()));
     }
 
-    @DeleteMapping("/opgroups/{optionGroupId}")
+    @DeleteMapping("/optiongroups/{optionGroupId}")
     @ResponseStatus(NO_CONTENT)
     public ApiResponse<Void> delete(@PathVariable Long optionGroupId,
                                     @AuthenticationPrincipal CustomUserDetails details) {
+        optionGroupService.delete(optionGroupId, details.getId());
         return ApiResponse.of(NO_CONTENT, null);
     }
+
+
 }
