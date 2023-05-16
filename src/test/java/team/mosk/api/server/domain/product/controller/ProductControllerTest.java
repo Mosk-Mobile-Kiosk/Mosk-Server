1918b5e734c5789eddb6be23582777615d0a9c51
Hounds1 <zoodisx@gmail.com>
2023-05-16 15:42:07 +0900

    Feat : @ActiveProfile 어노테이션 추가
    - 테스트 환경에서 defalut yml에 나눠놓은 windows, mac filePath가 적용되지 않는 현상으로인한 추가

--- src/test/java/team/mosk/api/server/domain/product/controller/ProductControllerTest.java
+++ src/test/java/team/mosk/api/server/domain/product/controller/ProductControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import team.mosk.api.server.domain.product.dto.*;
@@ -40,6 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class ProductControllerTest {
 
     @Autowired
